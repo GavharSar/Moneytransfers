941c225516777f2b16245b20f90dc822972d4dd9
Gavkhar Saryeva <eibova94@mail.ru>
2023-02-13 18:31:36 +0300

    edited the code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,27 @@
 fun main() {
     val amound = 10_000
-    var Commission = if (amound / 100 * 0.75 < 35 ) 35 else amound / 100 * 0.75
+    val minComission = 35
+    val maxComission = 0.0075
+    var Commission = if (amound * maxComission < minComission ) minComission else amound * maxComission
     println("Сумма комиссии состаяляет $Commission рублей")
     println()
 
     val likes = 65
-    var solution = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
+    val manText = "человеку"
+    val peopleText = "людям"
+    var solution = if (likes % 10 == 1 && likes % 100 != 11) manText else peopleText
     println("Понравилось $likes $solution")
     println()
 
-    var sum = 1500
-    val discount = 100
-    if (sum > 1000 && sum < 10_000) {
-        sum -= discount
-    } else if (sum > 10_000) {
-        sum = sum / 100 * 5
+    var sum = 10_001
+    val minDiscount = 100
+    val maxDiscount = 0.05
+    val minSum = 1000
+    val maxSum = 10_000
+    if (sum > minSum && sum <= maxSum) {
+        sum -= minDiscount
+    } else if (sum > maxSum) {
+        sum = (sum - (sum * maxDiscount)).toInt()
     }
     println("Сумма покупки составила $sum")
 
